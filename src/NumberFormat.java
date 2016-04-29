10afbc6628e7d18b6c3515f0da956ad56c2e10c7
srima karekal <srima589.karekal@gmail.com>
2016-04-29 11:54:27 +0530

    commit

---
+++ src/NumberFormat.java
@@ -0,0 +1,15 @@
+
+public class NumberFormat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+int num=Integer.parseInt("abc");
+System.out.println(num);
+		}
+		catch(NumberFormatException e){
+		System.out.println("Please understand,how can i convert string to number");
+		}
+	}
+
+}
